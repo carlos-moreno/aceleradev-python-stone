588e0306ea1a735c658f7e0f806db3a7aa4d3031
Carlos Moreno <omorenodovale@gmail.com>
2020-06-10 17:14:59 -0500

    Update test_main in modulo-V

--- modulo-V/test_main.py
+++ modulo-V/test_main.py
@@ -1,19 +1,34 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 import django
+
 django.setup()
 from django.test import TestCase
 from api.models import User, Agent, Group, Event, GroupUser
 
 
 class TestChallenge9(TestCase):
-
     def setUp(self):
-        user = User.objects.create(name="Jose", email="jose@gmail.com", password="xxxxxxxxxxxxxxxxxxxxxxx")
-        agent = Agent.objects.create(name="Machine1", address="192.168.1.1", status=True, env="prod", version="1.1.1")
+        user = User.objects.create(
+            name="Jose", email="jose@gmail.com", password="xxxxxxxxxxxxxxxxxxxxxxx"
+        )
+        agent = Agent.objects.create(
+            name="Machine1",
+            address="192.168.1.1",
+            status=True,
+            env="prod",
+            version="1.1.1",
+        )
         group = Group.objects.create(name="Admin")
         GroupUser.objects.create(user=user, group=group)
-        Event.objects.create(level="CRITICAL", data="django.core.exceptions.ValidationError", user=user, agent=agent, arquivado=False)
+        Event.objects.create(
+            level="CRITICAL",
+            data="django.core.exceptions.ValidationError",
+            user=user,
+            agent=agent,
+            arquivado=False,
+        )
 
     def test_1(self):
         user = User.objects.get(name="Jose")
@@ -30,3 +45,4 @@ class TestChallenge9(TestCase):
     def test_4(self):
         event = Event.objects.get(level="CRITICAL")
         self.assertEqual(event.level, "CRITICAL")
+
